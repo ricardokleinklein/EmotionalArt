6e2c18d3360fb9bddf4449e069157099c4328ea7
Ricardo Kleinlein <ricardo.kleinlein@upm.es>
2022-12-14 13:29:58 +0100

    Simplify preprocessing outcome - unit is painting

--- scripts/preprocess_artemis.py
+++ scripts/preprocess_artemis.py
@@ -21,6 +21,9 @@ import argparse
 import pandas
 
 from pathlib import Path
+from tqdm import tqdm
+from numpy.linalg import norm
+from sklearn.preprocessing import LabelBinarizer
 
 
 ART_DIR = "/mnt/HDD/DATA/ARTEMIS/artemis_official_data/official_data/wikiart"
@@ -39,6 +42,8 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("dst", type=str, help="Location to save new file")
     parser.add_argument("--img_root", type=str,
                         help="Image local directory", default=ART_DIR)
+    parser.add_argument("-q", "--quiet", help="Hide messages",
+                        action="store_true")
     return parser.parse_args()
 
 
@@ -80,14 +85,34 @@ def main() -> None:
     dataset = pandas.read_csv(file)
     dataset['utterance'] = dataset['utterance'].apply(
         lambda s: s + '.' if s[-1] != '.' else s
-    )
+    )   # Append a dot to the end of a sentence if it doesn't have one
+
+    # Merging all utterances to a single one for each artwork
+    # Also, turn individual emotion labels into probability distributions
+    unique_artworks = dataset['painting'].unique()
+    merged_utterances = []
+    label_bin = LabelBinarizer()
+    label_bin.fit(dataset['emotion'].unique())
+    emotion_dists = []
+    for artwork in tqdm(unique_artworks, total=len(unique_artworks),
+                        disable=args.quiet):
+        subset = dataset[dataset['painting'] == artwork]
+        merged_utterances.append(''.join(subset['utterance']))
+        emotion_count = label_bin.transform(subset['emotion']).sum(axis=0)
+        emotion_dists.append(emotion_count / emotion_count.sum())
+
+    dataset.drop_duplicates(subset='painting', inplace=True)
+    dataset['utterance'] = merged_utterances
+    dataset['emotion'] = emotion_dists
+
+    # Check all artworks are available locally
     dataset['localpath'] = dataset.apply(
         lambda x: absolute_local_path(img_root, x.art_style, x.painting),
-        axis=1)
+        axis=1)     # Obtain image paths in the local system
     dataset = dataset[dataset['localpath'].map(lambda s: s.exists())]
-    dataset['emotion_label'] = dataset['emotion'].apply(emotion_to_int)
-    dataset.sort_values('painting', ascending=True, inplace=True,
-                        ignore_index=True)
+
+    # Sort them alphabetically
+    dataset.sort_values(by='painting', inplace=True)
     dataset.to_csv(savedir, index=False)
 
 
