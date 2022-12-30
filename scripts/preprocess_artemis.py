2215276e6755ebcd358e6cf8fb498ad644b34466
Ricardo Kleinlein <ricardo.kleinlein@upm.es>
2022-12-30 10:03:19 +0100

    Agreement Threshold artemis

--- scripts/preprocess_artemis.py
+++ scripts/preprocess_artemis.py
@@ -13,6 +13,7 @@ Positional arguments:
 
 Optional arguments:
     --artdir        Local directory where images are saved
+    -a, --agreement     Filter out paintings whose
     --val           Val size
     --test          Test size
     -s, --seed      Random seed
@@ -45,6 +46,10 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("dst", type=str, help="Location to save new file")
     parser.add_argument("--artdir", type=str, default=ART_DIR,
                         help="Image local directory")
+    parser.add_argument("-a", "--agree", type=float, default=0.0,
+                        help="Paintings whose principal mode (emotion) has "
+                             "less than this probability density will be "
+                             "filtered out.")
     parser.add_argument("--val", type=float, default=0.15, help="Val size")
     parser.add_argument("--test", type=float, default=0.25, help="Test size")
     parser.add_argument("-s", "--seed", type=int, default=1234,
@@ -77,13 +82,13 @@ def main() -> None:
     if not file.exists():
         raise IOError(f"Artemis file not found in {file}")
 
-    dataset = pandas.read_csv(file)
+    dataset = pandas.read_csv(file)[:100]
 
     # Check all artworks are available locally
     dataset['localpath'] = dataset.apply(
         lambda x: absolute_local_path(img_root, x.art_style, x.painting),
         axis=1)     # Obtain image paths in the local system
-    # dataset = dataset[dataset['localpath'].map(lambda s: s.exists())]
+    dataset = dataset[dataset['localpath'].map(lambda s: s.exists())]
 
     # Append a dot to the end of a sentence if it doesn't have one
     dataset['utterance'] = dataset['utterance_spelled'].apply(
@@ -98,20 +103,29 @@ def main() -> None:
     label_bin.fit(dataset['emotion'].unique())
     emotion_dists = []
     most_emotion = []
+    under_agreement_lvl = []
     for artwork in tqdm(unique_artworks, total=len(unique_artworks),
                         disable=args.quiet):
         subset = dataset[dataset['painting'] == artwork]
-        if len(subset) < 2:
+        if len(subset) < 2: # samples with only 1 utterance
             subset = pandas.concat([subset, subset])
 
         merged_utterances.append(' '.join(subset['utterance']))
         emotion_count = label_bin.transform(subset['emotion']).sum(axis=0)
+        artwork_dist = emotion_count / emotion_count.sum()
+        if not numpy.max(artwork_dist) > args.agree:
+            under_agreement_lvl.append(subset.index[0])
         most_emotion.append(numpy.argmax(emotion_count))
-        emotion_dists.append(emotion_count / emotion_count.sum())
+        emotion_dists.append(artwork_dist)
+
     dataset.drop_duplicates(subset='painting', inplace=True)
     dataset['utterance'] = merged_utterances
     dataset['emotion'] = emotion_dists
     dataset['emotion_label'] = most_emotion
+
+    # Remove samples whose agreement between annotators is smaller than
+    # threshold
+    dataset.drop(under_agreement_lvl, inplace=True)
 
     # Select partitions stratified by majority emotion of the artwork
     dev, test = train_test_split(dataset.index, test_size=args.test,
